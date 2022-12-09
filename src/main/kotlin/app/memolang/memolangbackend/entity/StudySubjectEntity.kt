b93cb89813b7823ca39696de80f815699de01962
Pedram Hajesmaeeli <pedram.hajesmaeeli@glovoapp.com>
2022-12-09 01:20:40 +0100

    Add indexes, renaame columns

--- src/main/kotlin/app/memolang/memolangbackend/entity/StudySubjectEntity.kt
+++ src/main/kotlin/app/memolang/memolangbackend/entity/StudySubjectEntity.kt
@@ -3,20 +3,30 @@ package app.memolang.memolangbackend.entity
 import org.hibernate.annotations.CreationTimestamp
 import java.time.ZonedDateTime
 import javax.persistence.CascadeType
+import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.FetchType
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
+import javax.persistence.Index
 import javax.persistence.OneToMany
+import javax.persistence.Table
 
 @Entity
+@Table(
+    indexes = [
+        Index(columnList = "owner_username"),
+    ]
+)
 class StudySubjectEntity(
     @Id
     @GeneratedValue
     var id: Long? = null,
     var name: String? = null,
+    @Column(name = "owner_username")
     var ownerUsername: String? = null,
     @CreationTimestamp
+    @Column(name = "created_at")
     var createdAt: ZonedDateTime? = null,
     @OneToMany(
         cascade = [CascadeType.ALL],
